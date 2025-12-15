45d20f2069b5fb9edbc4d1b709191e2db7c76b5b
Guilherme China <echina725@gmail.com>
2025-12-14 21:15:51 -0300

    fix: annotation loaders

---
+++ backend/core/presentation/handlers/loader.ts
@@ -0,0 +1,8 @@
+import "./close-panel.handler";
+import "./dynamic-search.handler";
+import "./option-selected.handler";
+import "./preview-request.handler";
+import "./shiki-init-done.handler";
+import "./webview-dom-ready.handler";
+
+export function loadWebviewHandlers() {}
--- backend/extension.ts
+++ backend/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from "vscode";
+import { loadFuzzyProviders } from "./core/finders/loader";
 import { FuzzyFinderPanelController } from "./core/presentation/fuzzy-panel.controller";
+import { loadWebviewHandlers } from "./core/presentation/handlers/loader";
 import { Globals } from "./globals";
 import { getCmdId, registerAndSubscribeCmd } from "./utils/commands";
 import { getConfigurationSection } from "./utils/configuration";
@@ -11,6 +13,9 @@ export async function activate(context: vscode.ExtensionContext) {
   console.log(`${Globals.EXTENSION_NAME} activated!`);
   Globals.EXTENSION_URI = context.extensionUri;
   Globals.USER_THEME = getConfigurationSection(Globals.cfgSections.colorTheme, "Default Dark+");
+
+  loadFuzzyProviders();
+  loadWebviewHandlers();
 
   registerAndSubscribeCmd(
     getCmdId("fuzzy", "file"),
