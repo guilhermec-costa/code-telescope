b24ca423e8863f6a0d808bb118101c7d51afee4c
Guilherme China <echina725@gmail.com>
2025-12-20 15:50:16 -0300

    chore: config manager

--- backend/core/common/config-manager.ts
+++ backend/core/common/config-manager.ts
@@ -7,54 +7,31 @@ import {
 } from "../../../shared/exchange/extension-config";
 import { Globals } from "../../globals";
 
+enum ExtensionCfgSection {
+  WS_FILE_FINDER = "wsFileFinder",
+  WS_TEXT_FINDER = "wsTextFinder",
+  PREVIEW = "preview",
+  PANEL_SETUP = "panelSetup",
+}
+
 export class ExtensionConfigManager {
   private static get root() {
     return vscode.workspace.getConfiguration(Globals.EXTENSION_CONFIGURATION_PREFIX);
   }
 
   static get wsFileFinderCfg(): WsFileFinderConfig {
-    const { asRelativePath, excludeHidden, excludePatterns, includePatterns, maxResults } =
-      this.root.get<WsFileFinderConfig>("wsFileFinder")!;
-
-    return {
-      excludeHidden,
-      includePatterns,
-      excludePatterns,
-      maxResults,
-      asRelativePath,
-    };
+    return this.root.get<WsFileFinderConfig>(ExtensionCfgSection.WS_FILE_FINDER)!;
   }
 
   static get wsTextFinderCfg(): WsTextFinderConfig {
-    const { excludeHidden, excludePatterns, includePatterns, maxResults, maxColumns, maxFileSize } =
-      this.root.get<WsTextFinderConfig>("wsTextFinder")!;
-    return {
-      excludeHidden,
-      includePatterns,
-      excludePatterns,
-      maxResults,
-      maxColumns,
-      maxFileSize,
-    };
+    return this.root.get<WsTextFinderConfig>(ExtensionCfgSection.WS_TEXT_FINDER)!;
   }
 
   static get previewManagerCfg(): PreviewManagerConfig {
-    const { scrollBehavior, horizontalScrollFraction, verticalScrollFraction } =
-      this.root.get<PreviewManagerConfig>("preview")!;
-    return {
-      scrollBehavior,
-      horizontalScrollFraction,
-      verticalScrollFraction,
-    };
+    return this.root.get<PreviewManagerConfig>(ExtensionCfgSection.PREVIEW)!;
   }
 
   static get uiPanelCfg(): PanelSetupConfig {
-    const { leftSideWidthPct, rightSideWidthPct, panelContainerPct } = this.root.get<PanelSetupConfig>("panelSetup")!;
-
-    return {
-      leftSideWidthPct,
-      rightSideWidthPct,
-      panelContainerPct,
-    };
+    return this.root.get<PanelSetupConfig>(ExtensionCfgSection.PANEL_SETUP)!;
   }
 }
