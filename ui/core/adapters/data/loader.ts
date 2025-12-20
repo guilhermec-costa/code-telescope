17aeca303c7823959437bb7f1dfef85ab8338f4e
Guilherme China <echina725@gmail.com>
2025-12-20 16:43:26 -0300

    chore: rename ws files

--- ui/core/adapters/data/loader.ts
+++ ui/core/adapters/data/loader.ts
@@ -1,4 +1,4 @@
 import "./branch-finder.data-adapter";
 import "./git-commit-finder.data-adapter";
-import "./workspace-files-finder.data-adapter";
-import "./workspace-text-finder.data-adapter";
+import "./ws-files-finder.data-adapter";
+import "./ws-text-finder.data-adapter";
