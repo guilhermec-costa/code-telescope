d2e2b0f405c16cb6d4fe187cde68f52f1014e998
Guilherme China <echina725@gmail.com>
2026-01-13 23:16:07 -0300

    remove zoom change handler

--- backend/core/common/events/index.ts
+++ backend/core/common/events/index.ts
@@ -1,5 +1,4 @@
 import { ConfigChangeHandler } from "../../abstractions/config-change-handler";
 import { ThemeChangeHandler } from "./handlers/theme-change.handler";
-import { ZoomChangeHandler } from "./handlers/zoom-change.handler";
 
-export const CONFIG_CHANGE_HANDLERS: ConfigChangeHandler[] = [new ThemeChangeHandler(), new ZoomChangeHandler()];
+export const CONFIG_CHANGE_HANDLERS: ConfigChangeHandler[] = [new ThemeChangeHandler()];
