7297c14d8f4f7bfbcdb443b55fa132ea3ed70d15
Guilherme China <echina725@gmail.com>
2026-01-28 00:12:42 -0300

    fix: tests

--- backend/tests/presentation/handlers/close-panel.handler.test.ts
+++ backend/tests/presentation/handlers/close-panel.handler.test.ts
@@ -1,5 +1,8 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import type * as vscode from "vscode";
+import { WindowConfig } from "../../../../shared/exchange/extension-config";
+import { ExtensionConfigManager } from "../../../core/common/config-manager";
+import { FuzzyFinderPanelController } from "../../../core/presentation/fuzzy-panel.controller";
 import { ClosePanelHandler } from "../../../core/presentation/handlers/close-panel.handler";
 import { Globals } from "../../../globals";
 import { execCmd } from "../../../utils/commands";
@@ -25,8 +28,21 @@ describe("ClosePanelHandler", () => {
     webview = {} as vscode.Webview;
   });
 
-  it("closes active panel", async () => {
+  it("closes active panel and dispose the webview", async () => {
+    vi.spyOn(ExtensionConfigManager, "window", "get").mockReturnValue({
+      closeBehaviorOnClose: "dispose",
+    } as WindowConfig);
     await handler.handle({ type: "closePanel" }, webview);
     expect(execCmd).toHaveBeenCalledWith(Globals.cmds.focusActiveFile);
+    expect(FuzzyFinderPanelController.instance?.dispose).toHaveBeenCalled();
+  });
+
+  it("closes active panel and dot not dispose the webview", async () => {
+    vi.spyOn(ExtensionConfigManager, "window", "get").mockReturnValue({
+      closeBehaviorOnClose: "minimize",
+    } as WindowConfig);
+    await handler.handle({ type: "closePanel" }, webview);
+    expect(execCmd).toHaveBeenCalledWith(Globals.cmds.focusActiveFile);
+    expect(FuzzyFinderPanelController.instance?.dispose).not.toHaveBeenCalled();
   });
 });
--- backend/tests/presentation/handlers/option-selected.handler.test.ts
+++ backend/tests/presentation/handlers/option-selected.handler.test.ts
@@ -1,4 +1,6 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
+import { WindowConfig } from "../../../../shared/exchange/extension-config";
+import { ExtensionConfigManager } from "../../../core/common/config-manager";
 import { FuzzyFinderPanelController } from "../../../core/presentation/fuzzy-panel.controller";
 import { OptionSelectedHandler } from "../../../core/presentation/handlers/option-selected.handler";
 
@@ -20,11 +22,27 @@ describe("OptionSelectedHandler", () => {
     handler = new OptionSelectedHandler();
   });
 
-  it("request selected option data", async () => {
+  it("request selected option data and dispose the webview", async () => {
     const data = { anyKey: "anyValue" };
 
+    const extConfigSpy = vi.spyOn(ExtensionConfigManager, "window", "get").mockReturnValue({
+      closeBehaviorOnSelection: "dispose",
+    } as WindowConfig);
     await handler.handle({ type: "optionSelected", data });
 
     expect(FuzzyFinderPanelController.instance!.provider.onSelect).toHaveBeenCalledExactlyOnceWith(data);
+    expect(FuzzyFinderPanelController.instance?.dispose).toHaveBeenCalled();
+    expect(extConfigSpy).toHaveBeenCalled();
+  });
+
+  it("request selected option and dot not dispose the webview", async () => {
+    const data = { anyKey: "anyValue" };
+
+    vi.spyOn(ExtensionConfigManager, "window", "get").mockReturnValue({
+      closeBehaviorOnSelection: "minimize",
+    } as WindowConfig);
+    await handler.handle({ type: "optionSelected", data });
+
+    expect(FuzzyFinderPanelController.instance?.dispose).not.toHaveBeenCalled();
   });
 });
